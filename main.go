64fa332db029902c00ef6a7139cf4b105e77d1cb
Till Kottmann <me@deletescape.ch>
2020-07-21 16:56:43 +0200

    Add option to run a "dry run" and host info from shodan dl
    Signed-off-by: Till Kottmann <me@deletescape.ch>

--- main.go
+++ main.go
@@ -1,47 +1,121 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"flag"
 	"fmt"
 	sonargo "github.com/deletescape/sonargo/sonar"
 	"log"
+	"net/url"
 	"os"
 	"path"
 	"path/filepath"
 )
 
+var (
+	noDl       bool
+	shodanFile string
+)
+
+func init() {
+	flag.BoolVar(&noDl, "n", false, "Doesn't download discovered projects, and only prints info about them")
+	flag.StringVar(&shodanFile, "s", "", "Path to a Shodan download file with hosts to run against")
+}
+
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("usage:", os.Args[0], "[endpoint]")
-		fmt.Println("    example:", os.Args[0], "https://sonar.example.com/api")
+	flag.Parse()
+
+	if shodanFile == "" && flag.Arg(0) == "" {
+		fmt.Println("usage:", os.Args[0], "<options> [endpoint]")
+		fmt.Println("    example:", os.Args[0], "-n https://sonar.example.com/api")
 		os.Exit(1)
 	}
-	client, err := sonargo.NewAnonymousClient(os.Args[1])
+
+	if shodanFile == "" {
+		err := checkServer(flag.Arg(0))
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		info, err := os.Stat(shodanFile)
+		if os.IsNotExist(err) || info.IsDir() {
+			log.Fatalln("error: file", shodanFile, "is not a file or doesn't exist")
+		}
+
+		file, err := os.Open(shodanFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		scanner.Buffer(nil, 500000)
+		for scanner.Scan() {
+			var record ShodanRecord
+			if err := json.Unmarshal(scanner.Bytes(), &record); err != nil {
+				os.Stderr.WriteString(fmt.Sprintln("error:", err))
+				continue
+			}
+			baseUrl := &url.URL{
+				Scheme: record.Scheme(),
+				Host:   record.Host(),
+				Path:   "/api",
+			}
+			if noDl {
+				fmt.Printf("baseUrl %s\n", baseUrl)
+				record.Print()
+				fmt.Println("projects:")
+			}
+			if err := checkServer(baseUrl.String()); err != nil {
+				fmt.Println("---")
+				fmt.Println()
+				os.Stderr.WriteString(fmt.Sprintln("error:", err))
+				continue
+			}
+			fmt.Println("---")
+			fmt.Println()
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func checkServer(url string) error {
+	client, err := sonargo.NewAnonymousClient(url)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	v, _, err := client.Projects.Search(&sonargo.ProjectsSearchOption{
 		Ps: "500",
 	})
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	for _, c := range v.Components {
-		fmt.Println("Downloading", c.Key)
-		os.Mkdir(c.Key, os.ModePerm)
-		tree, _, err := client.Components.Tree(&sonargo.ComponentsTreeOption{
-			Component:  c.Key,
-			Ps:         "500",
-			S:          "qualifier,name",
-			Strategy:   "children",
-		})
-		if err != nil {
-			fmt.Printf("error: %v\n", err)
-			continue
+		if noDl {
+			fmt.Printf("    %s (%s)\n", c.Name, c.Key)
+		} else {
+			fmt.Println("Downloading", c.Key)
+			os.Mkdir(c.Key, os.ModePerm)
+			tree, _, err := client.Components.Tree(&sonargo.ComponentsTreeOption{
+				Component: c.Key,
+				Ps:        "500",
+				S:         "qualifier,name",
+				Strategy:  "children",
+			})
+			if err != nil {
+				fmt.Printf("error: %v\n", err)
+				continue
+			}
+			baseDir, _ := filepath.Abs(c.Key)
+			recurseTree(baseDir, client, tree.Components)
 		}
-		baseDir, _ := filepath.Abs(c.Key)
-		recurseTree(baseDir, client, tree.Components)
 	}
+	return nil
 }
 
 func recurseTree(dir string, client *sonargo.Client, components []*sonargo.Component) {
---
+++ model.go
@@ -0,0 +1,159 @@
+package main
+
+import "fmt"
+
+type ShodanRecord struct {
+	Shodan struct {
+		ID      string `json:"id"`
+		Options struct {
+		} `json:"options"`
+		Ptr     bool   `json:"ptr"`
+		Module  string `json:"module"`
+		Crawler string `json:"crawler"`
+	} `json:"_shodan"`
+	Product string      `json:"product"`
+	Hash    int         `json:"hash"`
+	Os      interface{} `json:"os"`
+	Opts    struct {
+		Vulns      []interface{} `json:"vulns"`
+		Heartbleed string        `json:"heartbleed"`
+	} `json:"opts"`
+	IP   int64  `json:"ip"`
+	Isp  string `json:"isp"`
+	HTTP struct {
+		HTMLHash   int `json:"html_hash"`
+		RobotsHash int `json:"robots_hash"`
+		Redirects  []struct {
+			Host     string `json:"host"`
+			Data     string `json:"data"`
+			Location string `json:"location"`
+		} `json:"redirects"`
+		Securitytxt interface{} `json:"securitytxt"`
+		Title       string      `json:"title"`
+		SitemapHash interface{} `json:"sitemap_hash"`
+		Robots      string      `json:"robots"`
+		Favicon     struct {
+			Hash     int    `json:"hash"`
+			Data     string `json:"data"`
+			Location string `json:"location"`
+		} `json:"favicon"`
+		Host       string `json:"host"`
+		HTML       string `json:"html"`
+		Location   string `json:"location"`
+		Components struct {
+			GitLab struct {
+				Categories []interface{} `json:"categories"`
+			} `json:"GitLab"`
+			Ruby struct {
+				Categories []interface{} `json:"categories"`
+			} `json:"Ruby"`
+			RubyOnRails struct {
+				Categories []interface{} `json:"categories"`
+			} `json:"Ruby on Rails"`
+			VueJs struct {
+				Categories []interface{} `json:"categories"`
+			} `json:"Vue.js"`
+		} `json:"components"`
+		Server          string      `json:"server"`
+		Sitemap         interface{} `json:"sitemap"`
+		SecuritytxtHash interface{} `json:"securitytxt_hash"`
+	} `json:"http"`
+	Cpe  []string `json:"cpe"`
+	Port int      `json:"port"`
+	Ssl  struct {
+		Dhparams interface{} `json:"dhparams"`
+		Tlsext   []struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+		} `json:"tlsext"`
+		Versions      []string      `json:"versions"`
+		AcceptableCas []interface{} `json:"acceptable_cas"`
+		Cert          struct {
+			SigAlg     string `json:"sig_alg"`
+			Issued     string `json:"issued"`
+			Expires    string `json:"expires"`
+			Expired    bool   `json:"expired"`
+			Version    int    `json:"version"`
+			Extensions []struct {
+				Critical bool   `json:"critical,omitempty"`
+				Data     string `json:"data"`
+				Name     string `json:"name"`
+			} `json:"extensions"`
+			Fingerprint struct {
+				Sha256 string `json:"sha256"`
+				Sha1   string `json:"sha1"`
+			} `json:"fingerprint"`
+			Serial  int64 `json:"serial"`
+			Subject struct {
+				CN string `json:"CN"`
+			} `json:"subject"`
+			Pubkey struct {
+				Type string `json:"type"`
+				Bits int    `json:"bits"`
+			} `json:"pubkey"`
+			Issuer struct {
+				C  string `json:"C"`
+				CN string `json:"CN"`
+				O  string `json:"O"`
+			} `json:"issuer"`
+		} `json:"cert"`
+		Cipher struct {
+			Version string `json:"version"`
+			Bits    int    `json:"bits"`
+			Name    string `json:"name"`
+		} `json:"cipher"`
+		Chain []string `json:"chain"`
+		Alpn  []string `json:"alpn"`
+	} `json:"ssl"`
+	Hostnames []string `json:"hostnames"`
+	Location  struct {
+		City         interface{} `json:"city"`
+		RegionCode   interface{} `json:"region_code"`
+		AreaCode     interface{} `json:"area_code"`
+		Longitude    float64     `json:"longitude"`
+		CountryCode3 interface{} `json:"country_code3"`
+		CountryName  string      `json:"country_name"`
+		PostalCode   interface{} `json:"postal_code"`
+		DmaCode      interface{} `json:"dma_code"`
+		CountryCode  string      `json:"country_code"`
+		Latitude     float64     `json:"latitude"`
+	} `json:"location"`
+	Timestamp string   `json:"timestamp"`
+	Domains   []string `json:"domains"`
+	Org       string   `json:"org"`
+	Data      string   `json:"data"`
+	Asn       string   `json:"asn"`
+	Transport string   `json:"transport"`
+	IPStr     string   `json:"ip_str"`
+}
+
+func (r *ShodanRecord) Scheme() string {
+	if r.Shodan.Crawler == "https" {
+		return "https"
+	}
+	return "http"
+}
+
+func (r *ShodanRecord) Host() string {
+	return fmt.Sprintf("%s:%s", r.HTTP.Host, r.Port)
+}
+
+func (r *ShodanRecord) Print() {
+	fmt.Printf("host: %s\n", r.HTTP.Host)
+	fmt.Printf("org: %s (%s)\n", r.Org, r.Asn)
+	if r.Shodan.Crawler == "https" {
+		fmt.Printf("ssl.cert.subject.cn: %s\n", r.Ssl.Cert.Subject.CN)
+		for _, e := range r.Ssl.Cert.Extensions {
+			if e.Name == "subjectAltName" {
+				fmt.Printf("ssl.cert.extensions[subjectAltName]: %s\n", e.Data)
+			}
+		}
+	}
+	if len(r.Hostnames) > 0 {
+		fmt.Printf("hostnames: \n")
+		for _, h := range r.Hostnames {
+			fmt.Printf("         - %s\n", h)
+		}
+	}
+	fmt.Println()
+}
